34b25396abc05357d7f59a6cd3155a6d2fef70f8
DefaltSimon <simon.peter.goricar@gmail.com>
2017-07-13 17:51:45 +0200

    Remove __init__, remove socket.io library, frontend updates

--- amber/frontend/assets/scripts/main.js
+++ amber/frontend/assets/scripts/main.js
@@ -1,56 +1,90 @@
-console.log("Connecting to " + host + ":" + port);
+/*
 
+Amber Engine
+
+ */
+
+// Utility functions
 function dumps(obj) {
     return JSON.stringify(obj);
 }
-
 function loads(string) {
     return JSON.parse(string);
 }
 
-// Setup socket.io
+// Setup the websocket
+console.log("Connecting to " + host + ":" + port);
 let socket = new WebSocket("ws://" + host + ":" + port);
 
 let socketQueueId = 0;
 let socketQueueCallback = {};
 
+const version = "0.1.0";
 
-function sendAction(action, data, callback) {
-    let payload = {
-        type: "action",
-        action: action,
-        data: data,
-        req_id: ++socketQueueId
-    };
-    let pl = dumps(payload);
-    socketQueueCallback[socketQueueId] = callback;
+class AmberFrontend {
+    sendHandshake() {
+        let data = {
+            uiVersion: version
+        };
 
-    socket.send(pl);
+        let cb = function (status, data) {
+            console.log("Handshake complete");
+            console.log(data);
+        };
+
+        this.sendEvent("handshake", data, cb);
+    }
+
+    sendAction(action, data, callback) {
+        if (data === null) {
+            data = {}
+        }
+
+
+        let payload = {
+            type: "action",
+            action: action,
+            data: data,
+            req_id: ++socketQueueId
+        };
+        let pl = dumps(payload);
+        socketQueueCallback[socketQueueId] = callback;
+
+        socket.send(pl);
+    }
+
+    sendEvent(event, data, callback) {
+        let payload = {
+            type: "event",
+            event: event,
+            data: data,
+            req_id: ++socketQueueId
+        };
+        let pl = dumps(payload);
+        socketQueueCallback[socketQueueId] = callback;
+
+        socket.send(pl);
+    }
+
+    // Game methods
+    getInventory(cb) {
+        this.sendAction("get-inventory", null, cb);
+    }
+
+    getRoomInfo(cb) {
+        this.sendAction("get-room-info", null, cb)
+    }
+
+    getLocations(cb) {
+        this.sendAction("get-locations", null, cb)
+    }
 }
 
-function sendHandshake(game_name) {
-    let payload = {
-        type: "event",
-        event: "handshake",
-        gameName: game_name,
-        req_id: ++socketQueueId
-    };
-    let pl = dumps(payload);
-    socketQueueCallback[socketQueueId] = function (status, data) {
-        console.log("Response got!");
-        console.log(status);
-        console.log(data);
-
-    };
-
-    socket.send(pl)
-}
-
+amber = new AmberFrontend();
 
 socket.onopen = function () {
-    console.log("Connected!");
-
-    sendHandshake("test game");
+    console.log("Websocket connected");
+    amber.sendHandshake();
 };
 
 socket.onmessage = function (evt) {
@@ -63,4 +97,12 @@ socket.onmessage = function (evt) {
     // Callback for that particular message gets called
     socketQueueCallback[c_id](status, data);
 
+};
+
+const btn = document.getElementById("testbtn");
+btn.onclick = function () {
+    amber.getLocations(function (status, data) {
+        console.log("Locations received!");
+        console.log(data);
+    })
 };
