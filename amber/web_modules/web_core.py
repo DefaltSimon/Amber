844ec3eafb3e7278abab2ffb55d03f2da3d43188
DefaltSimon <simon.peter.goricar@gmail.com>
2018-02-08 17:05:07 +0100

    Im stupid, more debugging

--- amber/web_modules/web_core.py
+++ amber/web_modules/web_core.py
@@ -57,11 +57,11 @@ def add_header(r):
 def simplify(url: str):
     if url.startswith(("assets/", "/assets")):
         l_path, filename = os.path.split(url)
-        log.info("HMMMMM " + os.path.join(FRONTEND_DIR, l_path))
+        log.info("\nHMMMMMM\n{}, {}\n{}".format(l_path, filename, os.path.join(FRONTEND_DIR, l_path)))
         return send_from_directory(os.path.join(FRONTEND_DIR, l_path), filename)
     else:
         f_path, fn = os.path.split(os.path.join(GAME_DIR, url))
-        log.info("HMMMMM2 " + os.path.join(FRONTEND_DIR, l_path))
+        log.info("\nHMMMMMM2\n{}, {}\n{}".format(f_path, fn, os.path.join(FRONTEND_DIR, f_path)))
         return send_from_directory(f_path, fn)
 
 
